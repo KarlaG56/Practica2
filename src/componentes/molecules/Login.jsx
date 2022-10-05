ae5864bbeaf37e2fd9b578cc58e3be96d7e402c0
KarlaG56 <213356@ids.upchiapas.edu.mx>
2022-10-05 01:55:23 -0500

    Diseño2

--- src/componentes/molecules/Login.jsx
+++ src/componentes/molecules/Login.jsx
@@ -1,12 +1,19 @@
-import InputT from "../atoms/Informacion";
+import InputT from "../atoms/Informacion"
 import Button from "../atoms/button";
+import "../../assets/styles/formulario.css"
 
 function Longin(){
     return (
         <div className="formulario">
+                <br/><br/><br/> <br/><br/><br/> <br/><br/><br/>
+            
             <form action="">
+                <h1>Iniciar sesion</h1>
+                <br/><br></br>
                 <InputT value={"name"} type={"text"} id={"name"} placeholder={"Nombre"}/>
+                
                 <InputT value={"password"} type={"password"} id={"password"} placeholder={"Contraseña"}/>
+                <br/>
                 <Button></Button>
             </form>
         </div>
